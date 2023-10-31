3d0e5c03976d510b5b67048a0ae592dbd2b9ec2f
TrabalhoUniderp <149090442+TrabalhoUniderp@users.noreply.github.com>
2023-10-31 18:54:35 -0400

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
-public class App {
+import Menu.Menu;
+
+public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    Menu E1 = new Menu();
+    E1.Executar();
     }
-}
+}        
