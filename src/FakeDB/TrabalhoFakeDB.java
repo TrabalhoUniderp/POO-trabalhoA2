9e92eef91e78bcf70cff480d7eea1a20acc16242
TrabalhoUniderp <149090442+TrabalhoUniderp@users.noreply.github.com>
2023-10-31 18:53:22 -0400

    Update TrabalhoFakeDB.java

--- src/FakeDB/TrabalhoFakeDB.java
+++ src/FakeDB/TrabalhoFakeDB.java
@@ -4,13 +4,14 @@ import dominio.Trabalho;
 
 public class TrabalhoFakeDB extends BaseFakeDB<Trabalho> {
 
+    DisciplinaFakeDB D = new DisciplinaFakeDB();
     @Override
     public void CarregarDados() {
 
-        this.tabela.add(new Trabalho(1, null, null));
-        this.tabela.add(new Trabalho(2, null, null));
-        this.tabela.add(new Trabalho(3, null, null));
-        this.tabela.add(new Trabalho(4, null, null));
+        this.tabela.add(new Trabalho(1, D.tabela.get(0), "trabalho 1"));
+        this.tabela.add(new Trabalho(2, D.tabela.get(0), "trabalho 2"));
+        this.tabela.add(new Trabalho(3,D.tabela.get(1), "trabalho 3"));
+        this.tabela.add(new Trabalho(4, D.tabela.get(1), "trabalho 4"));
 
     }
 
