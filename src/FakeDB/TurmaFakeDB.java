7857202beab6da3bbef59eb9a0a9c30412465fee
TrabalhoUniderp <149090442+TrabalhoUniderp@users.noreply.github.com>
2023-10-31 18:54:12 -0400

    Update TurmaFakeDB.java

--- src/FakeDB/TurmaFakeDB.java
+++ src/FakeDB/TurmaFakeDB.java
@@ -2,6 +2,9 @@ package FakeDB;
 
 import dominio.Turma;
 import dominio.Professor;
+
+import java.time.LocalDate;
+
 import dominio.Disciplina;
 
 public class TurmaFakeDB extends BaseFakeDB<Turma> {
@@ -9,15 +12,14 @@ public class TurmaFakeDB extends BaseFakeDB<Turma> {
     @Override
     protected void CarregarDados() {
 
-        Disciplina fisica = new Disciplina(0, "fisica", null);
         Disciplina ciencias = new Disciplina(0, "ciencias", null);
         Disciplina matematica = new Disciplina(0, "matematica", null);
-        Professor Luiz = new Professor(0, "Luiz", null, null, null, null);
-        Professor Alan = new Professor(0, "Alan", null, null, null, null);
+        Professor Luiz = new Professor(0, "Luiz", LocalDate.of(1994, 04, 05), "profLuiz", "senha321", "professor");
+        Professor Alan = new Professor(0, "Alan", LocalDate.of(1993, 12, 31), "profAlan", "senha123", "professor");
 
-        this.tabela.add(new Turma(1, matematica, Luiz, 4));
-        this.tabela.add(new Turma(2, ciencias, Alan, 4));
-        this.tabela.add(new Turma(3, fisica, Luiz, 4));
+        this.tabela.add(new Turma(1, matematica, ciencias, Luiz, Alan, 4));
+        this.tabela.add(new Turma(1, matematica, ciencias, Luiz, Alan, 4));
+        this.tabela.add(new Turma(1, matematica, ciencias, Luiz, Alan, 4));
     }
 
     public TurmaFakeDB() {
